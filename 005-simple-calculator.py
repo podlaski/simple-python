81a1183f71d29c8d58a696c96ee7ffca2c2353d0
podlaski <108882725+podlaski@users.noreply.github.com>
2022-12-23 13:25:28 +0100

    Create 005-simple-calculator.py

---
+++ 005-simple-calculator.py
@@ -0,0 +1,11 @@
+# Simple Calculator
+
+number1 = float(input('First number: '))
+number2 = float(input('Second number: '))
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+
+print(f'Addition: {addition} \nSubstraction: {subtraction} \nMultiplication: {multiplication} \nDivision: {division}')
