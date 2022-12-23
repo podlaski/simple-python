3221df52723b5fde11efa234314462006c50eb02
podlaski <108882725+podlaski@users.noreply.github.com>
2022-12-23 13:09:05 +0100

    Create 004-word-and-character-counter.py

---
+++ 004-word-and-character-counter.py
@@ -0,0 +1,10 @@
+# Counter (characters, words, averages) 
+
+text = input('Add text: ')
+characters = len(text) # with whitespaces
+characters2 = len(text.replace(' ','')) # without whitespaces
+words = len(text.split())
+average = characters/words # with whitespaces
+average2 = characters2/words # without whitespaces
+
+print(f"YOUR TEXT HAS\nCharacters with whitespaces: {characters}\nCharacters without whitespaces: {characters2} \nNumber of words: {words}\nAverage of characters per word (with whitespaces): {average}\nAverage of characters per word (without whitespaces): {average2}")
