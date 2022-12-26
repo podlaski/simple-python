a19388ae78bf97186250e0ff23a03f7bff9d8c1e
podlaski <108882725+podlaski@users.noreply.github.com>
2022-12-26 22:34:46 +0100

    add new elif text.isspace()

--- 009-letter-or-number-detector.py
+++ 009-letter-or-number-detector.py
@@ -6,5 +6,7 @@ if text.isalpha():
   print(f'{text} is the letter string')
 elif text.isnumeric():
   print(f'{text} is the number')
+elif text.isspace():
+  print({f'{text} is a whitespace. Please use only letters or only digits.'})
 else:
   print('Add correct value. You can use only letters or only digits.')
